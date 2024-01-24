810d0548d0e0d1fc2823c8837afb9a9a7a50b086
s23017 <s23017@std.it-college.ac.jp>
2024-01-24 07:14:51 +0000

    4.1/4.2 finish
    4.1と4.2完了

--- components/header.js
+++ components/header.js
@@ -1,10 +1,14 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
+
 const Header = () => {
   return (
     <header>
-      <Logo boxOn />
-      <Nav />
+      <div className={styles.flexContainer}>
+        <Logo boxOn />
+        <Nav />
+      </div>
     </header>
   )
 }
