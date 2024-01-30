f84b52d46eaa30bcf9a204cb9d3571a1e4a80d25
s23017 <s23017@std.it-college.ac.jp>
2024-01-30 06:26:07 +0000

    6.2-6.3finish
    6.2と6.3完了

---
+++ components/meta.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Meta = ({ pageTitle }) => {
+  return (
+    <Head>
+      <title>{pageTitle}</title>
+      <meta property='og:title' content={pageTitle} />
+    </Head>
+  )
+}
+
+export default Meta
--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -13,8 +14,8 @@ import eyecatch from 'images/about.jpg'
 const About = () => {
   return (
     <Container>
+      <Meta pageTitle='アバウト' />
       <Hero title='About' subtitle='About development activities' />
-
       <figure>
         <Image
           src={eyecatch}
