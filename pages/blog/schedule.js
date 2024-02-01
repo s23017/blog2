8c5d7046f343e9af04520b6be576fde58c78ec92
s23017 <s23017@std.it-college.ac.jp>
2024-02-01 06:15:52 +0000

    8.3 finish (mv next.config.mjs next.config.js)
    8.3 完了 next.config.mjsをnext.config.jsに変更

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,12 +1,25 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
+import Image from 'next/image'
 
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
     <Container>
       <article>
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='(min-width: 1152px) 1152px, 100vw'
+            priority
+          />
+        </figure>
       </article>
     </Container>
   )
@@ -24,5 +37,5 @@ const getStaticProps = async () => {
     }
   }
 }
-export { getStaticProps }
 export default Schedule
+export { getStaticProps }
