934bf678b478eb4783e4bcb34cf4af6ea35673dd
s23017 <s23017@std.it-college.ac.jp>
2024-02-07 03:10:14 +0000

    9.6finish
    9.6完了（前回のpushは9.5)

--- components/posts.js
+++ components/posts.js
@@ -1,12 +1,24 @@
 import styles from 'styles/posts.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Posts = ({ posts }) => {
   return (
     <div className={styles.gridContainer}>
-      {posts.map(({ title, slug }) => (
+      {posts.map(({ title, slug, eyecatch }) => (
         <article className={styles.post} key={slug}>
           <Link href={`/blog/${slug}`}>
+            <figure>
+              <Image
+                src={eyecatch.url}
+                alt=''
+                layout='fill'
+                objectFit='cover'
+                sizes='(min-width: 1152px) 576px, 50vw'
+                placeholder='blur'
+                blurDataURL={eyecatch.blurDataURL}
+              />
+            </figure>
             <h2>{title}</h2>
           </Link>
         </article>
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,10 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -16,6 +20,13 @@ const Blog = ({ posts }) => {
 const getStaticProps = async () => {
   const posts = await getAllPosts()
 
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
   return {
     props: {
       posts
