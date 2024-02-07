506f4675b76996fc3642a7422d8de69acd2d1562
s23017 <s23017@std.it-college.ac.jp>
2024-02-07 03:02:27 +0000

    9.6finish
    9.6完了

---
+++ components/posts.js
@@ -0,0 +1,18 @@
+import styles from 'styles/posts.module.css'
+import Link from 'next/link'
+
+const Posts = ({ posts }) => {
+  return (
+    <div className={styles.gridContainer}>
+      {posts.map(({ title, slug }) => (
+        <article className={styles.post} key={slug}>
+          <Link href={`/blog/${slug}`}>
+            <h2>{title}</h2>
+          </Link>
+        </article>
+      ))}
+    </div>
+  )
+}
+
+export default Posts
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,27 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' pageDesc='ブログの記事一覧' />
       <Hero title='Blog' subtitle='Recent Posts' />
+      <Posts posts={posts} />
     </Container>
   )
 }
+const getStaticProps = async () => {
+  const posts = await getAllPosts()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
 export default Blog
+export { getStaticProps }
